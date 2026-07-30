708adb2c2a252bfe1b574963124e67a57f641068
jjwwill09 <jjwwill09@gmail.com>
2026-07-30 11:48:50 -0500

    New best model

--- Model_Deployment/Neural_Network_Handler.py
+++ Model_Deployment/Neural_Network_Handler.py
@@ -22,7 +22,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Running Neural Network math on: {device}")
 
 # Load the model from the saved PyTorch weights
-checkpoint_path = "Model_Files/Farne_Back_Models/model_best.pth"
+checkpoint_path = "Model_Files/Farne_Back_Models/model_best_9.pth"
 checkpoint = torch.load(checkpoint_path, map_location=device)
 
 model = UNet().to(device)
